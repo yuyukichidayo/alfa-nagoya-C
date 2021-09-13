3881e618660340082b14f0b1d0956fb2a292d40f
yuyukichi <yuukichi777@outlook.jp>
2021-09-13 14:48:43 +0900

    エラー処理をテスト

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,14 +3,26 @@ let userName = "";
 btn.addEventListener("click", e => {
     userName = document.getElementById("nameInput").value;
     userNameGet(userName)
+    heavyAlert()
+
 });
 
 function userNameGet(userName) {
     if (userName == "") {
         alert("なまえを入力してね！")
     } else {
-        document.cookie = "nameData=hoge";
+        document.cookie = "nameData=" + userName;
         console.log(document.cookie)
+
     }
 
 };
+
+function heavyAlert() {
+    document.write('<img src="assets/images/main/error1.png" class="errors1" alt="エラーメッセージ">');
+    document.write('<img src="assets/images/main/error2.png" class="errors2" alt="エラーメッセージ">');
+    document.write('<img src="assets/images/main/error1.png" class="errors3" alt="エラーメッセージ">');
+    document.write('<img src="assets/images/main/error2.png" class="errors4" alt="エラーメッセージ">');
+    document.write('<img src="assets/images/main/error1.png" class="errors5" alt="エラーメッセージ">');
+    document.write('<img src="assets/images/main/error2.png" class="errors6" alt="エラーメッセージ">');
+}
