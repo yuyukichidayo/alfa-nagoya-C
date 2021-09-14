c9541c17ba66ba1688c72b8f8368247c17a1d70b
yuyukichi <yuukichi777@outlook.jp>
2021-09-14 20:26:55 +0900

    novelTemplateを実装

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,18 @@
 let userName = "";
 
+function main() {
+    getCookieData = document.cookie;
+    console.log(getCookieData)
+    if (getCookieData != "") {
+        console.log("get cokkie")
+        ending()
+    } else {
+        document.getElementById("nameInput").style.visibility = "visible";
+        document.getElementById("btn").style.visibility = "visible";
+        document.getElementById("title").style.visibility = "visible";
+    }
+}
+
 btn.addEventListener("click", e => {
     userName = document.getElementById("nameInput").value;
     userNameGet(userName)
@@ -10,18 +23,39 @@ function userNameGet(userName) {
     if (userName == "") {
         alert("なまえを入力してね！")
     } else {
-        document.cookie = "nameData=" + userName;
+        document.cookie = "nameData=" + userName + ";Max-Age=100";
         console.log(document.cookie)
-        heavyAlert()
+        alert("errorが発生しました。ユーザー情報が悪意のあるユーザーに取得されました。")
+        document.body.style.backgroundColor = "rgba(255,0,0,0.5)";
+        heavyAlert1()
+        setTimeout(heavyAlert2, 1000);
+        setTimeout(heavyAlert3, 2000);
+        setTimeout(heavyAlertEnd, 3000);
+        setTimeout(link, 6000)
     }
+}
+
+
+function link() {
+    location.href = "STEP1/STEP1-novelstart.html";
 
 };
 
-function heavyAlert() {
-    document.write('<img src="assets/images/main/error1.png" class="errors1" alt="エラーメッセージ">');
-    document.write('<img src="assets/images/main/error2.png" class="errors2" alt="エラーメッセージ">');
-    document.write('<img src="assets/images/main/error1.png" class="errors3" alt="エラーメッセージ">');
-    document.write('<img src="assets/images/main/error2.png" class="errors4" alt="エラーメッセージ">');
-    document.write('<img src="assets/images/main/error1.png" class="errors5" alt="エラーメッセージ">');
-    document.write('<img src="assets/images/main/error2.png" class="errors6" alt="エラーメッセージ">');
+function heavyAlert1() {
+    document.getElementById("error1").style.visibility = "visible";
+}
+
+function heavyAlert2() {
+    document.getElementById("error2").style.visibility = "visible";
+    document.getElementById("error3").style.visibility = "visible";
+}
+
+function heavyAlert3() {
+    document.getElementById("error4").style.visibility = "visible";
+    document.getElementById("error5").style.visibility = "visible";
+    document.getElementById("error6").style.visibility = "visible";
+}
+
+function heavyAlertEnd() {
+    document.getElementById("errorEnd").style.visibility = "visible";
 }
---
+++ assets/js/novelTemplate.js
@@ -0,0 +1,103 @@
+const provision = function() {
+    //引数は左から順に event, backgroundimg, leftimg, centerimg, rightimg, text
+    shine1 = new Shine(moneNameBox, blueback, mone, bag, bag, "The Egg 君は家に帰っている途中で死んだ。交通事故だった。")
+    shine2 = new Shine(moneNameBox, siverback, bag, mone, invisible, "ごく普通の交通事故だったが、致命的だった。")
+    shine3 = new Shine(moneNameBox, blueback, megane, mone, mone, "君は妻と子供二人を残した。死ぬときに痛みはなかった。")
+    shine4 = new Shine(moneNameBox, blueback, mone, bag, bag, "The Egg 君は家に帰っている途中で死んだ。交通事故だった。")
+    shine5 = new Shine(moneNameBox, blueback, mone, bag, bag, "The Egg 君は家に帰っている途中で死んだ。交通事故だった。")
+    shine6 = new Shine(moneNameBox, blueback, mone, bag, bag, "The Egg 君は家に帰っている途中で死んだ。交通事故だった。")
+    shine7 = new Shine(moneNameBox, blueback, mone, bag, bag, "The Egg 君は家に帰っている途中で死んだ。交通事故だった。")
+    shine8 = new Shine(moneNameBox, blueback, mone, bag, bag, "The Egg 君は家に帰っている途中で死んだ。交通事故だった。")
+    const shines = [shine1, shine2, shine3, shine4, shine5, shine6, shine7, shine8]
+    return shines
+}
+
+//ネームボックス一覧
+const moneNameBox = "../assets/images/itemImg/nameBox仮.png"
+
+//よく使う背景一覧
+const blueback = "../assets/images/backroundImg/blue21-scaled.jpg"
+const siverback = "../assets/images/backroundImg/siverimg.jpeg"
+
+//よく使うキャラクター一覧
+const mone = "../assets/images/charaImg/july_nikkori.png"
+const megane = "../assets/images/charaImg/メガネ.png"
+
+//よく使うアイテム一覧
+const invisible = "../assets/images/itemImg/null.png"
+const bag = "../assets/images/itemImg/kaban_kirei.png"
+
+
+class Shine {
+    constructor(nameBoximg, backgroundimg, leftimg, centerimg, rightimg, text) {
+        this.nameBoximg = nameBoximg;
+        this.backgroundimg = backgroundimg;
+        this.leftimg = leftimg;
+        this.centerimg = centerimg;
+        this.rightimg = rightimg;
+        this.text = text;
+    }
+};
+
+function nameBoximg(img) {
+    const nameBoximg = document.getElementById("nameBox");
+    nameBoximg.setAttribute("src", img)
+}
+
+function backgroundimg(img) {
+    document.body.background = img;
+}
+
+function leftimg(img) {
+    const leftimg = document.getElementById("leftimg");
+    leftimg.setAttribute("src", img)
+}
+
+function centerimg(img) {
+    const centerimg = document.getElementById("centerimg");
+    centerimg.setAttribute("src", img)
+}
+
+function rightimg(img) {
+    const rightimg = document.getElementById("rightimg");
+    rightimg.setAttribute("src", img)
+}
+
+function textMessage(text) {
+    const textelem = document.getElementById("textMessage");
+    textelem.innerText = text;
+}
+
+function shineexecution(shine) {
+    nameBoximg(shine.nameBoximg)
+    backgroundimg(shine.backgroundimg)
+    leftimg(shine.leftimg)
+    centerimg(shine.centerimg)
+    rightimg(shine.rightimg)
+    textMessage(shine.text)
+}
+
+const shines = provision();
+
+function main() {
+    shineexecution(shines[0]);
+}
+
+function clickExecution() {
+    //8の部分をshineの数にしてください
+    if (count > shines.length) {
+        //飛ばしたいリンクへ
+        location.href = "https://www.yahoo.co.jp/";
+    }
+    count++
+    console.log(count)
+    shineexecution(shines[count]);
+}
+
+var count = 0
+
+let clickCheck = document.getElementById('textBox');
+clickCheck.addEventListener('click', clickExecution)
+
+let clickheck2 = document.getElementById('textMessage');
+clickheck2.addEventListener('click', clickExecution)
