7b1f82ea0283c7d43269f7924240f9b3adbe3ad2
yuyukichi <yuukichi777@outlook.jp>
2021-09-17 10:48:55 +0900

    修正

--- assets/js/STEP1/step1NovelStart.js
+++ assets/js/STEP1/step1NovelStart.js
@@ -93,7 +93,7 @@ function clickExecution() {
     //8の部分をshineの数にしてください
     if (count > shines.length) {
         //飛ばしたいリンクへ
-        location.href = "STEP1/step1.htmll";
+        location.href = "../STEP1/step1.htmll";
     }
     count++
     console.log(count)
