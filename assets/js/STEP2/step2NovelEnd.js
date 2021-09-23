f2bf0c68734c2e38f1fbb226be9739cc0e914cd8
yuyukichi <yuukichi777@outlook.jp>
2021-09-23 21:39:31 +0900

    main修正

--- assets/js/STEP2/step2NovelEnd.js
+++ assets/js/STEP2/step2NovelEnd.js
@@ -23,13 +23,13 @@ const provision = function() {
     shine21 = new Shine(grayGooNameBox, mozaiku, mone_Shadow, invisible, grayGoo_do, "黙れ……黙れ黙れ黙れ黙れ！")
     shine22 = new Shine(moneNameBox, mozaiku, mone_Right, invisible, grayGoo_Shadow, "（さっき、つながりが切れたときに、君のデータにこっそりプロテクトを掛けておいたの）")
     shine23 = new Shine(moneNameBox, mozaiku, mone_Right, invisible, grayGoo_Shadow, "（だから、君のパソコンのデータはもうほぼ大丈夫。）")
-    shine25 = new Shine(moneNameBox, mozaiku, mone_Right, invisible, grayGoo_Shadow, "（後は仕上げのプログラムだけなんだけど、きみにお願いしてもいいかな？）")
+    shine24 = new Shine(moneNameBox, mozaiku, mone_Right, invisible, grayGoo_Shadow, "（後は仕上げのプログラムだけなんだけど、きみにお願いしてもいいかな？）")
     shine25 = new Shine(moneNameBox, mozaiku, mone_Right, invisible, grayGoo_Shadow, "（大丈夫。きみならできるよ。）")
-    shine25 = new Shine(moneNameBox, mozaiku, mone_Right, invisible, grayGoo_Shadow, "（わたし？　わたしは、クレイ・グーを……お父さんを止めなきゃ）")
-    shine25 = new Shine(moneNameBox, mozaiku, mone_Right, invisible, grayGoo_Shadow, "（さぁ、行って！）")
-    shine25 = new Shine(moneNameBox, mozaiku, mone_Right, invisible, grayGoo_Shadow, "......")
-    shine25 = new Shine(moneNameBox, mozaiku, mone_Right, invisible, grayGoo_Shadow, "じゃあね")
-    const shines = [shine1, shine2, shine3, shine4, shine5, shine6, shine7, shine8, shine9, shine10, shine11, shine12, shine13, shine14, shine15, shine16, shine17, shine18, shine19]
+    shine26 = new Shine(moneNameBox, mozaiku, mone_Right, invisible, grayGoo_Shadow, "（わたし？　わたしは、クレイ・グーを……お父さんを止めなきゃ）")
+    shine27 = new Shine(moneNameBox, mozaiku, mone_Right, invisible, grayGoo_Shadow, "（さぁ、行って！）")
+    shine28 = new Shine(moneNameBox, mozaiku, mone_Right, invisible, grayGoo_Shadow, "......")
+    shine29 = new Shine(moneNameBox, mozaiku, mone_Right, invisible, grayGoo_Shadow, "じゃあね")
+    const shines = [shine1, shine2, shine3, shine4, shine5, shine6, shine7, shine8, shine9, shine10, shine11, shine12, shine13, shine14, shine15, shine16, shine17, shine18, shine19, shine20, shine21, shine22, shine23, shine24, shine25, shine26, shine27, shine28, shine29]
     return shines
 }
 
